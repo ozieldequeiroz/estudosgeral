996255896b36e69fb5a2b16b78e0a2e9c9dafe06
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2022-10-19 22:49:20 -0300

    Update SpecificationFuncionario.java

--- spring-data/src/main/java/com/springdata/specification/SpecificationFuncionario.java
+++ spring-data/src/main/java/com/springdata/specification/SpecificationFuncionario.java
@@ -1,5 +1,31 @@
 package com.springdata.specification;
 
+import java.time.LocalDate;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import com.springdata.orm.Funcionario;
+
 public class SpecificationFuncionario {
+	
+	public static Specification<Funcionario> nome(String nome){
+		return (root,criteriaQuery,criteriaBuilder)->
+		criteriaBuilder.like(root.get("nome"), "%"+ nome+"%");
+	}
+	
+	public static Specification<Funcionario> cpf(String cpf){
+		return (root,criteriaQuery,criteriaBuilder)->
+		criteriaBuilder.like(root.get("cpf"), "%"+ cpf+"%");
+	}
+	
+	public static Specification<Funcionario> salario(Double salario){
+		return (root,criteriaQuery,criteriaBuilder)->
+		criteriaBuilder.greaterThan(root.get("salario"), salario);
+	}
+
+	public static Specification<Funcionario> cpf(LocalDate dataContratacao){
+		return (root,criteriaQuery,criteriaBuilder)->
+		criteriaBuilder.greaterThan(root.get("dataContratacao"),dataContratacao); 
+	}
 
 }
