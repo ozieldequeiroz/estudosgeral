c54e1e61e3430213df91de7aa6f0d84cc08e9d02
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2022-10-25 23:04:22 -0300

    Adicionando novas funcionalidades

--- spring-security/src/main/java/com/springsecurity/SpringSecurityApplication.java
+++ spring-security/src/main/java/com/springsecurity/SpringSecurityApplication.java
@@ -2,9 +2,15 @@ package com.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
+	
+	@GetMapping
+	public String hello() {
+		return "hello word!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityApplication.class, args);
