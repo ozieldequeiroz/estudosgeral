2f4525b2198124ed2fc9244bc971ce07fceb9348
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2022-11-02 12:10:01 -0300

    Atualizando versao

--- spring-security/src/main/java/com/springsecurity/conf/ApplicationSecurityConfig.java
+++ spring-security/src/main/java/com/springsecurity/conf/ApplicationSecurityConfig.java
@@ -1,5 +1,13 @@
 package com.springsecurity.conf;
 
-public class ApplicationSecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration 
+@EnableWebSecurity
+public class ApplicationSecurityConfig  {
+	
+
+	
 
 }
