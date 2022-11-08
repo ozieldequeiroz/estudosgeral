433188e8fffd3bc9929d0b9378f1a5c60761c619
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2022-11-07 23:00:31 -0300

    Add passwordcoding

--- spring-security/src/main/java/com/springsecurity/conf/ApplicationSecurityConfig.java
+++ spring-security/src/main/java/com/springsecurity/conf/ApplicationSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.springsecurity.conf;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -7,11 +8,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration 
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	private PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+		this.passwordEncoder = passwordEncoder;
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -31,7 +40,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		UserDetails annaSmithUser=User.builder()
 				.username("annasmith")
-				.password("12345")
+				.password(passwordEncoder.encode("12345"))
 				.roles("STUDENT")
 				.build();
 		return new InMemoryUserDetailsManager(annaSmithUser);
