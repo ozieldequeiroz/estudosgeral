2ec3cf5dc0b087868cf8b1274f021cf4f46476a3
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2022-11-18 23:13:31 -0300

    criando entidade-commit02

--- todo/src/main/java/com/todo/todo/TodoApplication.java
+++ todo/src/main/java/com/todo/todo/TodoApplication.java
@@ -2,12 +2,18 @@ package com.todo.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.todo.todo.model.ToDo;
 
 @SpringBootApplication
 public class TodoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
+		
 	}
 
 }
---
+++ todo/src/main/java/com/todo/todo/model/ToDo.java
@@ -0,0 +1,74 @@
+package com.todo.todo.model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+public class ToDo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column
+	private String description;
+	
+	@Column
+	private boolean done;
+	
+	@Column
+	private LocalDateTime createdDate;
+	
+	@Column
+	private LocalDateTime doneDate;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public boolean isDone() {
+		return done;
+	}
+
+	public void setDone(boolean done) {
+		this.done = done;
+	}
+
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public LocalDateTime getDoneDate() {
+		return doneDate;
+	}
+
+	public void setDoneDate(LocalDateTime doneDate) {
+		this.doneDate = doneDate;
+	}
+	
+}
