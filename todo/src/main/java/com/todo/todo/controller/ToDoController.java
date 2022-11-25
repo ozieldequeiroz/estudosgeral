267182b2fbcae87d13fa437f4313a27de68b6761
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2022-11-24 23:16:53 -0300

    adicionando novo metodo-commit06

--- todo/src/main/java/com/todo/todo/controller/ToDoController.java
+++ todo/src/main/java/com/todo/todo/controller/ToDoController.java
@@ -1,11 +1,13 @@
 package com.todo.todo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.todo.todo.model.ToDo;
 import com.todo.todo.repository.ToDoRepository;
@@ -20,6 +22,13 @@ public class ToDoController {
 	@PostMapping
 	public ToDo save(@RequestBody ToDo toDo) {
 		return repository.save(toDo);
+	}
+	
+	@GetMapping("{id}")
+	public ToDo getById(Long id) {
+		return repository
+				.findById(id)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 	}
 
 }
