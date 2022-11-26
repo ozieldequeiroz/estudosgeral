36c1cee76f574baf1766b101d91b7fc56feab27f
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2022-11-25 23:50:44 -0300

    Atualizando formato de datas

--- todo/src/main/java/com/todo/todo/model/ToDo.java
+++ todo/src/main/java/com/todo/todo/model/ToDo.java
@@ -2,12 +2,16 @@ package com.todo.todo.model;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.chrono.ChronoLocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -26,9 +30,11 @@ public class ToDo {
 	private boolean done;
 	
 	@Column
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
 	private LocalDateTime createdDate;
 	
 	@Column
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
 	private LocalDateTime doneDate;
 
 	public Long getId() {
@@ -69,6 +75,12 @@ public class ToDo {
 
 	public void setDoneDate(LocalDateTime doneDate) {
 		this.doneDate = doneDate;
+	}
+	
+	@PrePersist
+	public void beforeSave() {
+		this.setCreatedDate(LocalDateTime.now());
+		
 	}
 	
 }
