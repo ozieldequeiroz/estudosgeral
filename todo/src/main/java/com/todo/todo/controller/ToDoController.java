c5a7750cd475cb0aba5add7baaa8ea24a1ac5d1b
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2022-11-26 23:06:29 -0300

    Update ToDoController.java

--- todo/src/main/java/com/todo/todo/controller/ToDoController.java
+++ todo/src/main/java/com/todo/todo/controller/ToDoController.java
@@ -3,6 +3,7 @@ package com.todo.todo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,8 +25,8 @@ public class ToDoController {
 		return repository.save(toDo);
 	}
 	
-	@GetMapping("{id}")
-	public ToDo getById(Long id) {
+	@GetMapping("/{id}")
+	public ToDo getById(@PathVariable Long id) {
 		return repository
 				.findById(id)
 				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
