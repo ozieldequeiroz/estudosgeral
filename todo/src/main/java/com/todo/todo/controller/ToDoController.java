d1645e8f9dc58bf66eb001b0041ee89565f7b92e
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2022-11-30 22:21:09 -0300

    Update ToDoController.java

--- todo/src/main/java/com/todo/todo/controller/ToDoController.java
+++ todo/src/main/java/com/todo/todo/controller/ToDoController.java
@@ -1,7 +1,11 @@
 package com.todo.todo.controller;
 
+import java.util.Optional;
+import java.util.Scanner;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.server.handler.ResponseStatusExceptionHandler;
 
 import com.todo.todo.model.ToDo;
 import com.todo.todo.repository.ToDoRepository;
@@ -17,19 +22,45 @@ import com.todo.todo.repository.ToDoRepository;
 @RequestMapping("/api/todos")
 public class ToDoController {
 	
+	Scanner input = new Scanner(System.in);
+	
 	@Autowired
 	private ToDoRepository repository;
 	
 	@PostMapping
-	public ToDo save(@RequestBody ToDo toDo) {
-		return repository.save(toDo);
+	public ResponseEntity<ToDo> save(@RequestBody ToDo toDo) {
+		repository.save(toDo);
+		return ResponseEntity.ok(toDo);
 	}
 	
 	@GetMapping("/{id}")
-	public ToDo getById(@PathVariable Long id) {
-		return repository
-				.findById(id)
-				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+	public ResponseEntity<ToDo> getById(@PathVariable Long id) {
+		
+		Optional<ToDo>todo= repository.findById(id);
+		
+		if (todo.isPresent()) {
+		return ResponseEntity.ok().body(todo.get());
+		} else {
+		return	ResponseEntity.notFound().build();
+		}
+				
+	}
+	@PostMapping("/edit/{id}")
+	public ToDo edit(@PathVariable Long id) {
+		ToDo updateToDo = new ToDo();
+		Optional<ToDo> todo; 
+		todo = repository.findById(id);
+		
+		if(todo.isPresent()){
+			String newTask = input.next();
+			updateToDo.setId(id);
+			updateToDo.setTask(newTask);
+			repository.save(updateToDo);
+			return updateToDo;
+		} else {
+			return  todo.orElseThrow();
+		}
+		
 	}
 
 }
