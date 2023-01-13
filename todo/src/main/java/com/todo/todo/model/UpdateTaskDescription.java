b1d55707c5e926d2fc102225261e9ea1894055de
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2023-01-12 22:20:25 -0300

    corrigindo erro no banco

--- todo/src/main/java/com/todo/todo/model/UpdateTaskDescription.java
+++ todo/src/main/java/com/todo/todo/model/UpdateTaskDescription.java
@@ -13,7 +13,7 @@ import javax.validation.constraints.NotNull;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
-public class UpdateTaskDescription implements Serializable{
+public class UpdateTaskDescription implements Serializable {
 		
 	private static final long serialVersionUID = 1L;
 
