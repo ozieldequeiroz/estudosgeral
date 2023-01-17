c8b402cd22e8654a448ff4f00c8421d3afac985b
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2023-01-16 21:37:35 -0300

    Update WebSecurityConfig.java

--- todo/src/main/java/com/todo/todo/conf/WebSecurityConfig.java
+++ todo/src/main/java/com/todo/todo/conf/WebSecurityConfig.java
@@ -3,6 +3,8 @@ package com.todo.todo.conf;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -10,7 +12,22 @@ public class WebSecurityConfig {
 	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.httpBasic()
+			.and()
+			.authorizeHttpRequests()
+			.anyRequest()
+			.permitAll();
+			//.anyRequest().authenticated()
+			//.and()
+			//.csrf().disable();
+		
 		return http.build();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
