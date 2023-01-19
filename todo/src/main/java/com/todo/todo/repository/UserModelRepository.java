b8c3176262d634fa7b4abdc6a7f0e5b524cdfa23
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2023-01-18 22:25:47 -0300

    terminando seguranca jpa

--- todo/src/main/java/com/todo/todo/repository/UserModelRepository.java
+++ todo/src/main/java/com/todo/todo/repository/UserModelRepository.java
@@ -1,9 +1,13 @@
 package com.todo.todo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.todo.todo.model.UserModel;
 
 public interface UserModelRepository extends JpaRepository<UserModel, Long>{
+	
+	Optional<UserModel> findByName(String name);
 
 }
---
+++ todo/src/main/java/com/todo/todo/service/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.todo.todo.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.todo.todo.model.UserModel;
+import com.todo.todo.repository.UserModelRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	final UserModelRepository repository;
+
+	public UserDetailsServiceImpl(UserModelRepository repository) {
+		this.repository = repository;
+	}
+
+
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		UserModel user = repository.findByName(username)
+				.orElse( ()-> new UsernameNotFoundException("User not found"+username) );
+		return user;
+	}
+	
+	
+
+}
