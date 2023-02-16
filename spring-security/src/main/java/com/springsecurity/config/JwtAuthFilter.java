601233bc77a7b57bd617e0371e0df23fe0c7b029
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2023-02-15 22:27:15 -0300

    Create JwtAuthFilter.java

---
+++ spring-security/src/main/java/com/springsecurity/config/JwtAuthFilter.java
@@ -0,0 +1,26 @@
+package com.springsecurity.config;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.util.http.parser.Authorization;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter{
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		final String authHeader = request.getHeader("AUTHORIZATION");
+		final String userEmail;
+		final String jwtToken;
+		
+	}
+
+}
