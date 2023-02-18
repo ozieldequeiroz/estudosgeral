c4d519ff0927ef7f694a1e279516ea957b8a0e36
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2023-02-17 23:06:38 -0300

    Update JwtAuthFilter.java

--- spring-security/src/main/java/com/springsecurity/config/JwtAuthFilter.java
+++ spring-security/src/main/java/com/springsecurity/config/JwtAuthFilter.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.tomcat.util.http.parser.Authorization;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -20,6 +23,16 @@ public class JwtAuthFilter extends OncePerRequestFilter{
 		final String authHeader = request.getHeader("AUTHORIZATION");
 		final String userEmail;
 		final String jwtToken;
+		
+		if (authHeader == null || !authHeader.startsWith("Bearer")) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+		if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+			UserDetails userDetails = UserDetailsService.loadUserByUserName(userEmail);
+			final boolean isTokenValid;
+			
+		}
 		
 	}
 
