e3d98435c790424acc39d509ebb32de500ddb3e0
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2023-05-02 22:30:54 -0300

    Update project

--- delivery/src/main/java/com/app/delivery/DeliveryApplication.java
+++ delivery/src/main/java/com/app/delivery/DeliveryApplication.java
@@ -1,4 +1,4 @@
-package com.app.delivery.delivery;
+package com.app.delivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ delivery/src/main/java/com/app/delivery/api/controller/ClienteController.java
@@ -0,0 +1,5 @@
+package com.app.delivery.api.controller;
+
+public class ClienteController {
+
+}
