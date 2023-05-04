45e9fa4f915767acdcaa1247c69cc43cae674c98
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2023-05-03 21:49:23 -0300

    teste controller

--- delivery/src/main/java/com/app/delivery/api/controller/ClienteController.java
+++ delivery/src/main/java/com/app/delivery/api/controller/ClienteController.java
@@ -1,5 +1,14 @@
 package com.app.delivery.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ClienteController {
+	
+	@GetMapping("/clientes")
+	public String hello() {
+		return "Hello";
+	}
 
 }
