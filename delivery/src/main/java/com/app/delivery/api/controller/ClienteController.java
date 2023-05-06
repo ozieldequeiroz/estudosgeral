5dc2fbae8a2bbf14a94ec1931bf5668e83530006
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2023-05-06 15:43:38 -0300

    teste class

--- delivery/src/main/java/com/app/delivery/api/controller/ClienteController.java
+++ delivery/src/main/java/com/app/delivery/api/controller/ClienteController.java
@@ -1,14 +1,33 @@
 package com.app.delivery.api.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.app.delivery.domain.model.Cliente;
 
 @RestController
 public class ClienteController {
 	
 	@GetMapping("/clientes")
-	public String hello() {
-		return "Hello";
+	public List<Cliente> listar() {
+		var cliente1 = new Cliente();
+		cliente1.setId(1L);
+		cliente1.setNome("Joaõ dos nomes");
+		cliente1.setTelefone("83 956321477");
+		cliente1.setEmail("joaodosnomes@gmail.com");
+		
+		var cliente2 = new Cliente();
+		cliente2.setId(2L);
+		cliente2.setNome("beatriz sara");
+		cliente2.setTelefone("83 852321865");
+		cliente2.setEmail("beatrizsara@gmail.com");
+
+		
+		return Arrays.asList(cliente1,cliente2);
 	}
 
 }
--- delivery/src/main/java/com/app/delivery/domain/model/Cliente.java
+++ delivery/src/main/java/com/app/delivery/domain/model/Cliente.java
@@ -1,10 +1,5 @@
 package com.app.delivery.domain.model;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 
 public class Cliente {
 	
@@ -12,5 +7,30 @@ public class Cliente {
 	private String nome;
 	private String email;
 	private String telefone;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
 
 }
