519a6badc96676f73aac9230c17cb113c265b0c7
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2023-05-17 23:49:42 -0300

    update project

--- delivery/src/main/java/com/app/delivery/domain/model/Cliente.java
+++ delivery/src/main/java/com/app/delivery/domain/model/Cliente.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Id;
 @Entity
 public class Cliente {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
