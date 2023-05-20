9a58ffe8e5d95b862f8a94c39464193298340106
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2023-05-19 22:50:39 -0300

    create repository

--- delivery/src/main/java/com/app/delivery/api/controller/ClienteController.java
+++ delivery/src/main/java/com/app/delivery/api/controller/ClienteController.java
@@ -8,9 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.delivery.api.model.Cliente;
+import com.app.delivery.api.repository.ClienteRepository;
 
 @RestController
 public class ClienteController {
+	
+	private ClienteRepository clienteRespository;
 	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
--- delivery/src/main/java/com/app/delivery/api/repository/ClienteRepository.java
+++ delivery/src/main/java/com/app/delivery/api/repository/ClienteRepository.java
@@ -1,9 +1,11 @@
 package com.app.delivery.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.app.delivery.api.model.Cliente;
 
+@Repository
 public interface ClienteRepository extends JpaRepository<Cliente,Long> {
 
 }
