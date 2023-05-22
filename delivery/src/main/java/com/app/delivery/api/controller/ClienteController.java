a460e333fba09378c7700c371eef6a6bd79fc6b4
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2023-05-21 22:59:13 -0300

    update jpa crud commit01

--- delivery/src/main/java/com/app/delivery/api/controller/ClienteController.java
+++ delivery/src/main/java/com/app/delivery/api/controller/ClienteController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,24 +14,12 @@ import com.app.delivery.api.repository.ClienteRepository;
 @RestController
 public class ClienteController {
 	
+	@Autowired
 	private ClienteRepository clienteRespository;
 	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Joaõ dos nomes");
-		cliente1.setTelefone("83 956321477");
-		cliente1.setEmail("joaodosnomes@gmail.com");
-		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("beatriz sara");
-		cliente2.setTelefone("83 852321865");
-		cliente2.setEmail("beatrizsara@gmail.com");
-
-		
-		return Arrays.asList(cliente1,cliente2);
+		return clienteRespository.findAll();
 	}
 
 }
