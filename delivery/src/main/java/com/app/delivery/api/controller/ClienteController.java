93519795089d1f2eacb79a3be7bce75e154fe6ff
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2023-05-30 23:44:45 -0300

    Update ClienteController.java

--- delivery/src/main/java/com/app/delivery/api/controller/ClienteController.java
+++ delivery/src/main/java/com/app/delivery/api/controller/ClienteController.java
@@ -10,27 +10,34 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.delivery.api.model.Cliente;
 import com.app.delivery.api.repository.ClienteRepository;
 
 @RestController
+@RequestMapping("clientes")
 public class ClienteController {
 	
 	@Autowired
 	private ClienteRepository clienteRespository;
 	
-	@GetMapping("/clientes")
+	@GetMapping
 	public List<Cliente> listar() {
 		return clienteRespository.findAll();
 	}
 	
-	@GetMapping("/clientes/{id}")
+	@GetMapping("/{id}")
 	public ResponseEntity<Cliente> pegarCliente(@PathVariable Long id) {
 		return clienteRespository.findById(id)
 				.map(cliente ->ResponseEntity.ok(cliente))
 				.orElse(ResponseEntity.notFound().build());
 	}
-
+	
+	@PostMapping
+	public void criaCliente() {
+		
+	}
 }
