5ab1f7990999b09d2061e7a91704775c6b5a2def
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2023-06-01 23:18:24 -0300

    Update ClienteController.java

--- delivery/src/main/java/com/app/delivery/api/controller/ClienteController.java
+++ delivery/src/main/java/com/app/delivery/api/controller/ClienteController.java
@@ -7,12 +7,14 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.delivery.api.model.Cliente;
@@ -38,6 +40,7 @@ public class ClienteController {
 	}
 	
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente criaCliente(@RequestBody Cliente cliente) {
 		return clienteRespository.save(cliente);
 	}
