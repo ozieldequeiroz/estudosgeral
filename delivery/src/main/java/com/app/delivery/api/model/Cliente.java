af274c2a3c60d85b724025ed1bab2e5c1b0a0dd9
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2023-06-08 00:07:27 -0300

    add validation

--- delivery/src/main/java/com/app/delivery/api/model/Cliente.java
+++ delivery/src/main/java/com/app/delivery/api/model/Cliente.java
@@ -4,14 +4,23 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Cliente {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY )
 	private Long id;
+	@NotBlank
+	@Size(max=60)
 	private String nome;
+	@NotBlank
+	@Email	
 	private String email;
+	@NotBlank
+	@Size(max=20)
 	private String telefone;
 	
 	public Long getId() {
