136372f63c4a865a2c576f41636673e6cb48c90e
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2023-06-10 23:33:57 -0300

    Update ApiExceptionHandler.java

--- delivery/src/main/java/com/app/delivery/api/exceptionhandler/ApiExceptionHandler.java
+++ delivery/src/main/java/com/app/delivery/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,9 +1,21 @@
 package com.app.delivery.api.exceptionhandler;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		return super.handleMethodArgumentNotValid(ex, headers, status, request);
+	}
 
 }
