bf3599926f8d2233de9e9dd8a954ebd1bd721638
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2023-06-19 22:46:17 -0300

    Update ApiExceptionHandler.java

--- delivery/src/main/java/com/app/delivery/api/exceptionhandler/ApiExceptionHandler.java
+++ delivery/src/main/java/com/app/delivery/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.app.delivery.api.exceptionhandler;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
@@ -17,10 +19,13 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		List<Problema.Campo> campos = new ArrayList<>();
+		
 		Problema problema = new Problema();
 		problema.setStatus(status.value());
 		problema.setDataHora(LocalDateTime.now());
 		problema.setTitulo("Um ou mais campos invalidos. Faça o preenchimento novamente!");
+		problema.setCampos(campos);
 		
 		return super.handleExceptionInternal(ex,problema, headers, status, request);
 	}
