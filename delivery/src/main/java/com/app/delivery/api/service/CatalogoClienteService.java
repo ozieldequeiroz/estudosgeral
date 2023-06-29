28d844485e2f174af17ffc6e3b6e8e466c3618d2
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2023-06-28 23:24:44 -0300

    Update CatalogoClienteService.java

--- delivery/src/main/java/com/app/delivery/api/service/CatalogoClienteService.java
+++ delivery/src/main/java/com/app/delivery/api/service/CatalogoClienteService.java
@@ -1,8 +1,28 @@
 package com.app.delivery.api.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.app.delivery.api.model.Cliente;
+import com.app.delivery.api.repository.ClienteRepository;
 
 @Service
 public class CatalogoClienteService {
+
+	private ClienteRepository clienteRepository;
+	
+	public CatalogoClienteService(ClienteRepository clienteRepository) {
+		this.clienteRepository = clienteRepository;
+	}
+	
+	@Transactional
+	public Cliente salvar(Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+	
+	@Transactional
+	public void excluir(Long clienteId) {
+		 clienteRepository.deleteById(clienteId);
+	}
 
 }
