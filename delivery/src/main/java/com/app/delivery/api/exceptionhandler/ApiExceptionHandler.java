d72c195da1f7a73c23c0913c5326a99d79b55709
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2023-07-07 22:26:12 -0300

    Update ApiExceptionHandler.java

--- delivery/src/main/java/com/app/delivery/api/exceptionhandler/ApiExceptionHandler.java
+++ delivery/src/main/java/com/app/delivery/api/exceptionhandler/ApiExceptionHandler.java
@@ -8,12 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -42,5 +44,13 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		
 		return super.handleExceptionInternal(ex,problema, headers, status, request);
 	}
-
+	@ExceptionHandler(NegocioException.class)
+	public ResponseEntity<Object> handleNegotion(NegocioException nex, WebRequest request){
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		
+		Problema problema = new Problema();
+		problema.setStatus(status.value());
+		problema.setDataHora(LocalDateTime.now());
+		return handleExceptionInternal(nex, problema, new HttpHeaders(), status, request);
+	}
 }
