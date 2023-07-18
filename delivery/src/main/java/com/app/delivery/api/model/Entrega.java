79062ce2f835eb55a41f52c421f010b6bdd223d2
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2023-07-17 22:29:47 -0300

    Update Entrega.java

--- delivery/src/main/java/com/app/delivery/api/model/Entrega.java
+++ delivery/src/main/java/com/app/delivery/api/model/Entrega.java
@@ -1,8 +1,36 @@
 package com.app.delivery.api.model;
 
-public class Entrega {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+public class Entrega {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY )
 	private Long id;
+	
 	private Cliente cliente;
 	private Destinatario destinatario;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	public Destinatario getDestinatario() {
+		return destinatario;
+	}
+	public void setDestinatario(Destinatario destinatario) {
+		this.destinatario = destinatario;
+	}
+	
 }
