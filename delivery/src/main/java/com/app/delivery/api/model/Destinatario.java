b36902f1f4989026fa729a441c298dfbd1a0b539
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2023-07-18 22:39:48 -0300

    Update Destinatario.java

--- delivery/src/main/java/com/app/delivery/api/model/Destinatario.java
+++ delivery/src/main/java/com/app/delivery/api/model/Destinatario.java
@@ -1,5 +1,11 @@
 package com.app.delivery.api.model;
 
 public class Destinatario {
+	
+	private String nome;
+	private String logradouro;
+	private String numero;
+	private String complemento;
+	private String bairro;
 
 }
