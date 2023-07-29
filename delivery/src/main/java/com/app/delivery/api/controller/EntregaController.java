c71c7cf523e3db7ab762b29158b5b562be512195
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2023-07-29 00:00:40 -0300

    Update EntregaController.java

--- delivery/src/main/java/com/app/delivery/api/controller/EntregaController.java
+++ delivery/src/main/java/com/app/delivery/api/controller/EntregaController.java
@@ -1,8 +1,10 @@
 package com.app.delivery.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.delivery.api.model.Entrega;
@@ -16,6 +18,7 @@ public class EntregaController {
 	private SolicitacaoEntregaService entregaService;
 	
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public Entrega solicitacao(Entrega entrega) {
 		return entregaService.solicitar(entrega);
 	}
