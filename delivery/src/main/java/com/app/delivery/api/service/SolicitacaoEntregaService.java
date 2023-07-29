d11e401aa0ed2974495953be45478c782a52c2fd
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2023-07-28 23:55:19 -0300

    Update SolicitacaoEntregaService.java

--- delivery/src/main/java/com/app/delivery/api/service/SolicitacaoEntregaService.java
+++ delivery/src/main/java/com/app/delivery/api/service/SolicitacaoEntregaService.java
@@ -1,10 +1,13 @@
 package com.app.delivery.api.service;
 
+import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.app.delivery.api.model.Entrega;
+import com.app.delivery.api.model.StatusEntrega;
 import com.app.delivery.api.repository.EntregaRepository;
 
 @Service
@@ -15,6 +18,8 @@ public class SolicitacaoEntregaService {
 	
 	@Transactional
 	public Entrega solicitar(Entrega entrega) {
+		entrega.setStatus(StatusEntrega.PENDENTE);
+		entrega.setDataPedido(LocalDateTime.now());
 		return entregaRespository.save(entrega);
 	}
 }
