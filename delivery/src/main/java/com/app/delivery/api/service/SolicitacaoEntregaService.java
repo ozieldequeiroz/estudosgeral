b7c10a7706521acc7bc26891ae549a95012d52a0
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2023-08-04 23:12:38 -0300

    Update SolicitacaoEntregaService.java

--- delivery/src/main/java/com/app/delivery/api/service/SolicitacaoEntregaService.java
+++ delivery/src/main/java/com/app/delivery/api/service/SolicitacaoEntregaService.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.app.delivery.api.exceptionhandler.NegocioException;
+import com.app.delivery.api.model.Cliente;
 import com.app.delivery.api.model.Entrega;
 import com.app.delivery.api.model.StatusEntrega;
+import com.app.delivery.api.repository.ClienteRepository;
 import com.app.delivery.api.repository.EntregaRepository;
 
 @Service
@@ -15,9 +18,13 @@ public class SolicitacaoEntregaService {
 	
 	@Autowired
 	private EntregaRepository entregaRespository;
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	@Transactional
 	public Entrega solicitar(Entrega entrega) {
+		Cliente cliente = clienteRepository.findById(entrega.getCliente().getId())
+				.orElseThrow(()->new NegocioException("Cliente não encontrado"));
 		entrega.setStatus(StatusEntrega.PENDENTE);
 		entrega.setDataPedido(LocalDateTime.now());
 		return entregaRespository.save(entrega);
