f87fcfec2040b0874261b6e470fa4738dd8c2076
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2023-08-18 23:32:21 -0300

    Update Cliente.java

--- delivery/src/main/java/com/app/delivery/api/model/Cliente.java
+++ delivery/src/main/java/com/app/delivery/api/model/Cliente.java
@@ -1,15 +1,22 @@
 package com.app.delivery.api.model;
 
+import io.micrometer.common.lang.NonNull;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import jakarta.validation.groups.Default;
 
 @Entity
 public class Cliente {
+	
+	
+	@NotNull(groups = Default.class)
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY )
 	private Long id;
