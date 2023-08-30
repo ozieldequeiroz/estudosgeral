599ec57d2af3d86e47a0ba8c134c463b1da6f1aa
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2023-08-29 22:55:50 -0300

    Update EntregaController.java

--- delivery/src/main/java/com/app/delivery/api/controller/EntregaController.java
+++ delivery/src/main/java/com/app/delivery/api/controller/EntregaController.java
@@ -1,7 +1,12 @@
 package com.app.delivery.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.delivery.api.model.Entrega;
+import com.app.delivery.api.repository.EntregaRepository;
 import com.app.delivery.api.service.SolicitacaoEntregaService;
 
 import jakarta.validation.Valid;
@@ -19,11 +25,23 @@ public class EntregaController {
 	
 	@Autowired
 	private SolicitacaoEntregaService entregaService;
+	@Autowired
+	private EntregaRepository entregaRepository;
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Entrega solicitacao(@Valid @RequestBody Entrega entrega) {
+	public Entrega solicitacao(@Valid  @RequestBody Entrega entrega) {
 		return entregaService.solicitar(entrega);
 	}
 
+	@GetMapping
+	public List<Entrega>listarEntregas(){
+		return entregaRepository.findAll();
+	}
+	
+	public ResponseEntity<Entrega>buscar(@PathVariable Long entregaId){
+		return entregaRepository.findById(entregaId)
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+	}
 }
