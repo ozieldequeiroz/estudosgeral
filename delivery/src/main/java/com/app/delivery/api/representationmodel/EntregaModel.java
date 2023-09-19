833ed5aac0c6ac4f6f600bb3fc10c0e7e3b046e9
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2023-09-18 22:50:45 -0300

    Update EntregaModel.java

--- delivery/src/main/java/com/app/delivery/api/representationmodel/EntregaModel.java
+++ delivery/src/main/java/com/app/delivery/api/representationmodel/EntregaModel.java
@@ -1,5 +1,16 @@
 package com.app.delivery.api.representationmodel;
 
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+import com.app.delivery.api.model.StatusEntrega;
+
 public class EntregaModel {
+	private Long id;
+	private String nomeCliente;
+	private BigDecimal taxa;
+	private StatusEntrega status;
+	private OffsetDateTime dataPedido;
+	private OffsetDateTime dataFinalizacao;
 
 }
